74409b83fec1e19ded0c02d983c77a195fe6b682
Andres Lazo <andreslazo.e@gmail.com>
2026-03-04 23:43:47 -0600

    Implementación CRUD completo del módulo Clientes

--- SistemaGestionCitas/Data/ApplicationDbContext.cs
+++ SistemaGestionCitas/Data/ApplicationDbContext.cs
@@ -1,10 +1,12 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using SistemaGestionCitas.Models;
 
 namespace SistemaGestionCitas.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Cliente> Clientes { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ SistemaGestionCitas/Models/Cliente.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SistemaGestionCitas.Models
+{
+    public class Cliente
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Identificación")]
+        public string Identificacion { get; set; }
+
+        [Required]
+        [Display(Name = "Nombre Completo")]
+        public string NombreCompleto { get; set; }
+
+        [Required]
+        [Phone]
+        public string Telefono { get; set; }
+
+        [EmailAddress]
+        public string Correo { get; set; }
+    }
+}
